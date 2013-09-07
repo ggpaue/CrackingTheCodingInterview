fb4034be1f711e5a06e2c46181d06f6aabf0afa1
Guanpeng Gao <ggpaue@gmail.com>
2013-09-06 23:51:11 -0400

    sort a stack with a temp stack

--- Chapter 3/3-6.java
+++ Chapter 3/3-6.java
@@ -1,4 +1,11 @@
-
+/**
+ * 
+ * Write a program to sort a stack in ascending order(with biggest items on top).
+ * You may use additional stacks to hold items, but you may not copy the elements into any other data structure(such as an array).
+ * The stack supports the following operations: push, pop, peek, and isEmpty.
+ * 
+ */
+ 
 public class Solution {
 	public static ARStack sortStack(ARStack stack) {
 		ARStack sorted = new ARStack();
