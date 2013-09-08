c331119d2d51bbae59c2e17179b98182e767d23a
ggpaue <ggpaue@gmail.com>
2013-09-08 02:44:10 -0400

    used for 4-2

---
+++ Chapter 4/Node.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ * @author GGPAUE
+ * 
+ * Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
+ * 
+ */
+
+public class Node {
+	private Node adjacent[];
+	public int adjacentCount;
+	private String vertex;
+	public State state;
+	
+	public Node(String vertex, int adjacentLength) {
+		this.vertex = vertex;
+		adjacentCount = 0;
+		adjacent = new Node[adjacentLength];
+	}
+	
+	public void addAdjacent(Node x) {
+		if(adjacentCount < 30) {
+			this.adjacent[adjacentCount] = x;
+			adjacentCount++;
+		} else {
+			System.out.print("No more adjacent can be added");
+		}
+	}
+	
+	public Node[] getAdjacent() {
+		return adjacent;
+	}
+	
+	public String getVertex() {
+		return vertex;
+	}
+
+}
