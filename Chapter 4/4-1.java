b67f14897cf51a5ae49c955de512dcbf922c28ba
Guanpeng Gao <ggpaue@gmail.com>
2013-09-08 00:46:22 -0400

    check BST balanced

--- Chapter 4/4-1.java
+++ Chapter 4/4-1.java
@@ -1,4 +1,10 @@
-
+/**
+ * 
+ * Implement a function to check if a binary tree is balanced.
+ * For the purpose of this question, a balanced tree is defined to be a tree such that the height of the two subtrees of any node never differ by more than one.
+ * 
+ */
+ 
 public class Solution {
 	int val;
 	TreeNode left;
